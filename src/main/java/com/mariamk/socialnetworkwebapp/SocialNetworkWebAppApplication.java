dde24c124f5abb86e682031c844f87153d8e1d73
MariamKatamashvili <katamashvili.mariam@kiu.edu.ge>
2023-10-11 18:41:37 +0400

    created project

---
+++ src/main/java/com/mariamk/socialnetworkwebapp/SocialNetworkWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.mariamk.socialnetworkwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mariamk/socialnetworkwebapp/SocialNetworkWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariamk.socialnetworkwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
